d892cb73178d84b4c4f8c89c5888f14d35858ca1
burakyucel0 <burakuycl@gmail.com>
2022-06-10 13:00:10 +0300

    Edited

--- MVCdbConnect/MVCdbConnect/Controllers/HomeController.cs
+++ MVCdbConnect/MVCdbConnect/Controllers/HomeController.cs
@@ -6,26 +6,36 @@ namespace MVCdbConnect.Controllers
     public class HomeController : Controller
     {
         SiemensContext siemensContext = new SiemensContext();
-
+        [HttpGet]
         public IActionResult Index()
 
         {
-                     
-
             List<Suppliers> suppliersList = siemensContext.Suppliers.ToList();
-
-
-
             return View(suppliersList);
         }
 
+        [HttpGet]
         public IActionResult Detail(int id)
         {
-
             var idSup = siemensContext.Suppliers.FirstOrDefault(q => q.Id == id);
 
-
             return View(idSup);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add( Suppliers suppliers)
+        {
+
+            siemensContext.Add(suppliers);
+            siemensContext.SaveChanges();
+
+            return View();
         }
     }
 }
